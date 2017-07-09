2157ff49ea534b5c416a6ef226bafc17472876f2
olekar <olekar>
2017-07-10 00:47:58 +0300

    Removed Leftovers

--- VTFvsSSBO/common.cpp
+++ VTFvsSSBO/common.cpp
@@ -8,19 +8,12 @@ namespace demo {
   std::string text;
   bool stop;
 
-  #define DEMO2
+  #define DEMO1
 
   #ifdef DEMO1
       //Base Subroutine Test Demo
-      const char DEMO_NAME[] = "Subroutine Test";
+      const char DEMO_NAME[] = "Vertex Texture Fetch";
       #include "demo0.inl"
   #endif // DEMO1
-
-  #ifdef DEMO2
-      //Advance Subroutine Material Test Demo
-      const char DEMO_NAME[] = "Adv Subroutine Material Test";
-      #include "demo1.inl"
-  #endif // DEMO1
-
 
 } //namespace demo
